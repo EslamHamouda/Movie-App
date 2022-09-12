f263813750ebecb2eab47e63b0a9c881f17a2e10
Eslam Hamouda Ali <69871727+EslamHamouda@users.noreply.github.com>
2022-09-12 21:39:56 +0200

    update home screen, add search fragment

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.replace
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.movieapp.databinding.ActivityMainBinding
+import com.example.movieapp.home.ui.HomeFragment
+import com.example.movieapp.search.ui.SearchFragment
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHomeFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController=navHomeFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
+
     }
 }
---
+++ app/src/main/java/com/example/movieapp/search/ui/SearchFragment.kt
@@ -0,0 +1,57 @@
+package com.example.movieapp.search.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
+import com.example.movieapp.R
+import com.example.movieapp.databinding.FragmentSearchBinding
+import com.example.moviestorenew.home.data.DataSource
+
+class SearchFragment : Fragment() {
+    lateinit var binding:FragmentSearchBinding
+    lateinit var adabter:MoviesSearchAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding=FragmentSearchBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adabter=MoviesSearchAdapter(DataSource.getMovies())
+        binding.rvSearchResult.adapter=adabter
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                if(query!="") {
+                    adabter.changeList(query)
+                    binding.tvNumberOfItems.text = "${adabter.itemCount} records available"
+                }
+                else{
+                    adabter.setEmpty()
+                    binding.tvNumberOfItems.text = "0 records available"
+                }
+                return true
+            }
+
+        })
+    }
+
+}

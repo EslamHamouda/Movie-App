5dd10984463ed47a0726920c120099929bcf5e83
Eslam Hamouda Ali <69871727+EslamHamouda@users.noreply.github.com>
2022-09-13 21:48:51 +0200

    add login, register

---
+++ app/src/main/java/com/example/movieapp/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.movieapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            goOnboarding()
+        }, 3000)
+    }
+    private fun goOnboarding() {
+        startActivity(Intent(this, AuthActivity::class.java))
+        this.finish()
+    }
+}
